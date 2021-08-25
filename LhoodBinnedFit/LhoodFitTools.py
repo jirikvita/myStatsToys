30c840290ba2158d67b865255fe444954d47ad7b
Jiri Kvita <jiri.kvita@cern.ch>
2021-08-25 15:19:33 +0200

    lhood tools and central limit check

---
+++ LhoodBinnedFit/LhoodFitTools.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+
+# jk 24.8.2021
+
+import ROOT
+
+from math import log, exp, pow
+
+gsigVals = []
+gbgVals = []
+gdataVals = []
+
+# number of studied variables = free parameters
+gN = 1
+
+#################################
+def getPoissLog(k, nu):
+    plog = 0.
+    # nu is the expected=true
+    # k is the data
+    # P(nu,k) = exp(-nu) nu^k / k!
+    if nu <= 0.:
+        return 0.
+    plog = -nu + k*log(nu) # drop the constant data-dependent term (data is given!): -log(factorial(k))
+    return plog
+
+#################################
+def getNegLogLhood(mu):
+    lhoodLog = 0.
+    for i in range(0, len(gsigVals)):
+        lhoodLog = lhoodLog - getPoissLog(gdataVals[i], mu*gsigVals[i] + gbgVals[i])
+    return lhoodLog
+
+
+#################################
+# for Minuit:
+# https://root-forum.cern.ch/t/numerical-minimization-with-root-math-functor/13286/2
+
+class MyFunction( ROOT.TPyMultiGenFunction ):
+    def __init__( self ):
+        print "CREATED"
+        ROOT.TPyMultiGenFunction.__init__( self, self )
+
+    def NDim( self ):
+        print 'PYTHON NDim called'
+        return gN
+
+    def DoEval(self, args):
+        lhood = 0.
+        # TO CHECK!!!
+        mu = args[0]
+        negloglhood = getNegLogLhood(mu)
+        return negloglhood
+
+#################################
+def minimizeLhood(step, mumin, mumax):
+
+    mu = 0.5 * (mumax + mumin)
+    muerr = -1
+    # https://root-forum.cern.ch/t/numerical-minimization-with-root-math-functor/13286#p58684
+
+    # not returning errors:
+    # minimizer = ROOT.Math.Factory.CreateMinimizer("GSLMultiMin", "BFGS")
+    minimizer = ROOT.Math.Factory.CreateMinimizer('Minuit2', 'Minuit2' )
+    minimizer.SetMaxFunctionCalls(1000000)
+    minimizer.SetMaxIterations(100000)
+    minimizer.SetTolerance(0.001)
+    minimizer.SetPrintLevel(1)
+    
+    myfun = MyFunction()
+    # https://root.cern.ch/doc/master/classTMinuitMinimizer.html#a069cdb1a4cf6e2f373832a4cb094c6d2
+    minimizer.SetLimitedVariable(0, "mu", mu, step, mumin, mumax)
+    minimizer.SetFunction(myfun)
+
+    # minimize the -log(L)!;-)
+    minimizer.Minimize()
+    status = minimizer.Status()
+
+    # get the minimized parameter:
+    if status == 0:
+        print 'minimizeLhood :: SUCCESSFUL MINIMIZATION! ;-)'
+        mu =  minimizer.X()[0]
+        muerr = minimizer.Errors()[0]
+        #print('minimizeLhood :: Lhood fit result: mu={} +/- {}'.format(mu, muerr))
+    else:
+        print 'minimizeLhood :: FAILED MINIMIZATION! :-('
+        mu = -1
+    return mu, muerr
+
+
+#################################
+# support both 1D and 2D versions on demand
+def fitSignalStrength(hsig, hbg, hdata, is1D, step = 0.01, mumin = 0., mumax = 2.):
+
+    del gsigVals[:]
+    del gbgVals[:]
+    del gdataVals[:]
+    
+    # using lists as storage ;-)
+    # 1D:
+    if is1D:
+        for i in range(1, hdata.GetNbinsX()+1):
+            gsigVals.append(hsig.GetBinContent(i))
+            gbgVals.append(hbg.GetBinContent(i))
+            gdataVals.append(hdata.GetBinContent(i))
+    else:
+        # 2D:
+        for i in range(1, hdata.GetNbinsX()+1):
+            for j in range(1, hdata.GetNbinsY()+1):
+                gsigVals.append(hsig.GetBinContent(i,j))
+                gbgVals.append(hbg.GetBinContent(i,j))
+                gdataVals.append(hdata.GetBinContent(i,j))
+            
+    mu, muerr = minimizeLhood(step, mumin, mumax)
+    return mu, muerr
+
+#################################
+def ComputeZeroCompatibility(mu, muerr):
+    #hmu = dummmodel.Clone('muh')
+    #hmu.Reset()
+    #hmu.SetBinContent(1, mu)
+    #hmu.SetBinError(1, muerr)
+    #p0 = hmu.Chi2Test(dummmodel, 'WW') # by default returns the pval
+    p0 = -1
+    if muerr > 0:
+        p0 = ROOT.TMath.Prob(pow(mu/muerr, 2), 1)
+    
+    t0 = 999.
+    logt0 = 999.
+    if p0 > 0:
+        t0 = -log(p0)
+        if t0 > 0.:
+            logt0 = log(t0)
+    return p0, logt0
+            
+#################################
+#################################
+#################################
---
+++ centralLimitTheorem/cltCheck.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# Wed 25 Aug 15:01:14 CEST 2021
+
+from __future__ import print_function
+
+import ROOT
+from math import sqrt, pow, log, exp
+import os, sys, getopt
+
+cans = []
+stuff = []
+hs = []
+
+##########################################
+# https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+def main(argv):
+    #if len(sys.argv) > 1:
+    #  foo = sys.argv[1]
+
+    ### https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+    ### https://pymotw.com/2/getopt/
+    ### https://docs.python.org/3.1/library/getopt.html
+    gBatch = False
+    gTag=''
+    print(argv[1:])
+    try:
+        # options that require an argument should be followed by a colon (:).
+        opts, args = getopt.getopt(argv[2:], 'hbt:', ['help','batch','tag='])
+
+        print('Got options:')
+        print(opts)
+        print(args)
+    except getopt.GetoptError:
+        print('Parsing...')
+        print ('Command line argument error!')
+        print('{:} [ -h -b --batch -tTag --tag="MyCoolTag"]]'.format(argv[0]))
+        sys.exit(2)
+    for opt,arg in opts:
+        print('Processing command line option {} {}'.format(opt,arg))
+        if opt == '-h':
+            print('{:} [ -h -b --batch -tTag --tag="MyCoolTag"]'.format(argv[0]))
+            sys.exit()
+        elif opt in ("-b", "--batch"):
+            gBatch = True
+        elif opt in ("-t", "--tag"):
+            gTag = arg
+            print('OK, using user-defined histograms tag for output pngs {:}'.format(gTag,) )
+
+    if gBatch:
+        ROOT.gROOT.SetBatch(1)
+
+    print('*** Settings:')
+    print('tag={:}, batch={:}'.format(gTag, gBatch))
+
+    canname = 'can'
+    can = ROOT.TCanvas(canname, canname)
+
+    rnd = ROOT.TRandom3()
+    
+    cans.append(can)
+    # number of counts of some counting experiment
+    count = 250
+    # number of events for each random variable
+    Nx = 500
+    # number of random variables:
+    Ns = [2, 10, 100, 1000]
+    for N in Ns:
+        hname = 'aver{}'.format(N)
+        nbins = 100
+        x1 = 0.85 * count
+        x2 = 1.15*count
+        h1 = ROOT.TH1D(hname, hname, nbins, x1, x2)
+        hs.append(h1)
+        Ys = []
+        for ivar in range(0, N):
+            ys = []
+            for i in range(0, Nx):
+                ys.append(rnd.Poisson(count))
+            Ys.append(ys)
+        for i in range(0, len(Ys[0])):
+            aver = 0
+            for iy in range(0, len(Ys)):
+                aver = aver + Ys[iy][i]
+            aver = aver / N
+            h1.Fill(aver)
+    can.Divide(2,2)
+    for h1 in hs:
+        can.cd(hs.index(h1)+1)
+        h1.Draw('hist')
+        
+    ROOT.gApplication.Run()
+    return
+
+###################################
+###################################
+###################################
+
+if __name__ == "__main__":
+    # execute only if run as a script"
+    main(sys.argv)
+    
+###################################
+###################################
+###################################
+
