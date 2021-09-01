e1c684d97e26c60bc20395e34e773c66a1044286
Jiri Kvita <jiri.kvita@cern.ch>
2021-09-01 17:00:17 +0200

    stable

--- Gravity/gravity.py
+++ Gravity/gravity.py
@@ -3,39 +3,80 @@
 
 # na pocest meho ucitele doc. RNDr. Jiri Boka, CSc.
 
+# todo: odtransfrmovat teziste?
+# draw 1AU
+
+
 from __future__ import print_function
 
 import ROOT
-from math import sqrt, pow, log, exp, sin, cos, pi
+from math import sqrt, pow, log, exp, sin, cos, pi, log10, log
 import os, sys, getopt
 
 cans = []
 stuff = []
 
 
+#########################################
+
+def GetDr(x,y):
+    dr = 0.
+    for xx,yy in zip(x,y):
+        dr = dr + pow(xx-yy, 2)
+    if dr > 0:
+        dr = sqrt(dr)
+    return dr
+
+
+#########################################
+def MakeDigitStr(i, digits = 4):
+    tag = str(i)
+    n = digits
+    try: 
+        n = int(log10(i))
+    except ValueError:
+        pass
+    if i is 0:
+        n = 0
+    for i in range(0, digits - n):
+        tag = '0' + tag
+    return tag
+
 ########################################################################################
 
+kday = 24*3600. # seconds
 kAU = 149597871e3 # m
-kEarthMass = 5.972e24
-kSunMass = kEarthMass*333000.
+kEarthMass = 5.972e24 # kg
+kSunMass = 1.989e30 # kg
 kms = 1000. # 1 km/s = 1000 m/s
 # grav. const
-kappa = 6.67408e10-11 # m3 kg-1 s-2
+kappa = 6.67408e-11 # m3 kg-1 s-2
 
 # 3D world:
 kdim = 3
+
+kepsilon = 1.e-2*kAU
+
+kSunName = 'Sun'
+
 
 ########################################################################################
 
 # planets holder, to make sure they have unique ID etc;-)
 class cSystem:
-    def __init__(self, name, x0, y0, z0, cw = 800, ch = 100):
+    def __init__(self, name, x0, y0, z0, SF, i = 0, j = 0, cw = 400, ch = 400, printstep = 1000):
         self.name = name
         self.X0 = x0
         self.Y0 = y0
         self.Z0 = z0
+        self.SF = SF
         self.planets = []
-        self.can = ROOT.TCanvas(name, name, 0, 0, cw, ch)
+        self.can = ROOT.TCanvas(name, name, i, j, cw, ch)
+        self.can.Draw()
+        self.iteration = 0
+        self.printstep = printstep
+    def Inc(self):
+        self.iteration = self.iteration + 1
     def CheckUniquePlanetsNamesOK(self, planets):
         ip1 = -1
         for p1 in self.planets:
@@ -51,20 +92,46 @@ class cSystem:
                     return False
         return True
 
-    def DrawPlanet(self, planet):
+
+    def Print(self):
+        for planet in self.planets:
+            planet.Print()
+        return
+    
+    def DrawPlanetTrack(self, planet):
         # TODO
+        self.can.cd()
+        
+        # TODO:
+        # in the future: keep only finie numer of lines
+        # so shift them after some time
+        # draw last few dashed
+        # draw first few thicker
+        #lx1, ly1, lx2, ly2 = planet.x[0]/self.SF, planet.x[1]/self.SF, planet.oldx[0]/self.SF, planet.oldx[1]/self.SF
+        #print('lines coors: ', lx1, ly1, lx2, ly2)
+        #planet.lines.append(ROOT.TLine(lx1, ly1, lx2, ly2))
+        #line = planet.lines[-1]        
+        #line.SetLineColor(planet.mcol)
+        #line.SetLineWidth(2)
+        #line.SetLineStyle(1)
+        #line.SetNDC()
+        #line.Draw()
+        if planet.name != kSunName:
+            planet.Draw(self.SF)
         return
 
-    def Draw(self):
+    def DrawTracks(self):
         for planet in self.planets:
-            self.DrawPlanet(planet)
+            self.DrawPlanetTrack(planet)
+        if self.iteration % self.printstep == 0:
+            self.can.Print(self.name + '{}.png'.format(MakeDigitStr(self.iteration, 4)))
         return
 
 
 ########################################################################################
 # we assume circular motion, so the velocity is perp. to diameter vector
 class cPlanet:
-    def __init__(self, name, pid,  X0, Y0, Z0, mass, v0, R, theta, phi, mcol, mst = 20, msz = 1):
+    def __init__(self, name, pid,  X0, Y0, Z0, mass, v0, R, theta, phi, mcol, mst = 20, msz = 0.3):
         self.name = name
         self.pid = pid
         self.mass = mass
@@ -72,19 +139,31 @@ class cPlanet:
         self.R = R
         self.theta = theta 
         self.phi = phi
+        self.mcol = mcol
+        self.mst = mst
+        self.msz = msz
 
-        self.vr = 0.
-        self.vphi = v0
-        self.vtheta = 0.
+        self.time = 0.
+        self.timePrinted = False
+        
+        self.lines = []
+        self.markers = []
 
         self.x = [X0 + self.R*cos(self.phi)*sin(self.theta),
                   Y0 + self.R*sin(self.phi)*sin(self.theta),
-                  Z0 +  self.R*cos(self.theta)]
-        self.v = [0., 0., 0.]
-        self.oldx = [0., 0., 0.]
+                  Z0 + self.R*cos(self.theta)]
+
+        # initial position for period extraction
+        self.x0 = [1.*self.x[0], 1.*self.x[1], 1.*self.x[2], ]
         
-        self.RecomputePolar()
- 
+        # perp vector to initial R:
+        self.v = [-self.v0*sin(self.phi)*sin(self.theta),
+                  +self.v0*cos(self.phi)*sin(self.theta),
+                  0.]
+        self.oldx = [0., 0., 0.]
+        self.Archive()
+        self.Print()
+        
     def GetR2(self):
         R2 = 0.
         for i in range(0,kdim):
@@ -101,6 +180,20 @@ class cPlanet:
         self.x[2] = self.R*cos(self.theta)
         return self.x[2]
 
+    def Print(self):
+        print('{} id={} m={} x={} y={} z={} AU; vx={} vy={} vz={} km/s'.format(self.name, self.pid, self.mass,
+                                                                               self.x[0]/kAU, self.x[1]/kAU, self.x[2]/kAU,
+                                                                               self.v[0]/kms, self.v[1]/kms, self.v[2]/kms) )
+        return
+
+    def Draw(self, SF):
+        self.markers.append(ROOT.TMarker( self.x[0]/SF, self.x[1]/SF, self.mst) )
+        mark = self.markers[-1]
+        mark.SetMarkerSize(self.msz)
+        mark.SetMarkerColor(self.mcol)
+        mark.SetNDC()
+        mark.Draw()
+        
     def Archive(self):
         for i in range(0, kdim):
             self.oldx[i] = 1.*self.x[i]
@@ -109,6 +202,13 @@ class cPlanet:
     def RecomputePolar(self):
         self.Archive()
         self.R = self.GetR2()
+        if self.R > 0:
+            self.R = sqrt(self.R)
+        
+        # not needed so far?
+        # TODO!!!
+        # tantheta = 
+              
         return self.R, self.theta, self.phi
         
     def RecomputeCartesian(self):
@@ -116,17 +216,7 @@ class cPlanet:
         x,y,z = self.X(), self.Y(), self(Z)
         return x,y,z
 
-    # iunitial speed:
-    # to finish!!! inital v pepr. to r!
-    def vx(self):
-        self.vx = self.vr*cos(self.phi)*sin(self.theta)
-        return vx
-    def vy(self):
-        self.vy = self.vr*sin(self.phi)*sin(self.theta)
-        return self.vy
-    def vz(self):
-        self.vz = self.vr*cos(self.theta)
-        return self.vz
+   # use velocity in radial coors?
 
 
 ########################################################################################
@@ -135,34 +225,41 @@ class cPlanet:
 
 def MakeSolarSystem():
 
-    
-
-
     # phi's are random
     # theta is 0
 
-    X0 = 0.5
-    Y0 = 0.5
-    Z0 = 0.0
-
+    relSF = 5.
+    SF = relSF * kAU
+    
+    X0 = relSF/2.*kAU
+    Y0 = relSF/2.*kAU
+    Z0 = 0.0*kAU
+    
+    system = cSystem("Solar", X0, Y0, Z0, SF, 0, 0)
+    
+    theta = pi/2.
+    
     # the Sun
-    Sun = cPlanet('Sun', 0, X0, Y0, Z0, kSunMass, 0.*kms, 0*kAU, 0., 0., ROOT.kYellow, 20, 5.)
+    Sun = cPlanet(kSunName, 0, X0, Y0, Z0, kSunMass, 0.*kms, 0*kAU, theta, 0., ROOT.kYellow, 20, 4.)
+    Sun.Draw(SF)
     
     # inner:
-    Mercury = cPlanet('Mercury', 1, X0, Y0, Z0, 0.0553*kEarthMass, 29.4*kms, 0.387*kAU, 0.,  pi/4, ROOT.kRed, 20, 2.)
-    Venus   = cPlanet('Venus',   2, X0, Y0, Z0, 0.815*kEarthMass, 21.8*kms,  0.723*kAU, 0., -pi/4, ROOT.kRed, 20, 2.)
-    Earth   = cPlanet('Sun',     3, X0, Y0, Z0,       kEarthMass, 18.5*kms,  1.000*kAU, 0.,  pi/2, ROOT.kGreen+2, 20, 2.)
-    Mars    = cPlanet('Mars',    4, X0, Y0, Z0, 0.107*kEarthMass, 15.0*kms,  1.520*kAU, 0., -pi/2, ROOT.kRed, 20, 2.)
+    Mercury = cPlanet('Mercury', 1, X0, Y0, Z0, 0.0553*kEarthMass, 47.4*kms, 0.387*kAU, theta,  pi/4, ROOT.kBlack, 20)
+    Venus   = cPlanet('Venus',   2, X0, Y0, Z0, 0.815*kEarthMass,  35.0*kms,  0.723*kAU, theta, -pi/4, ROOT.kOrange, 20)
+    Earth   = cPlanet('Earth',   3, X0, Y0, Z0,       kEarthMass,  29.8*kms,  1.000*kAU, theta,  pi/4, ROOT.kGreen+2, 20)
+    Mars    = cPlanet('Mars',    4, X0, Y0, Z0, 0.107*kEarthMass,  24.1*kms,  1.520*kAU, theta, -pi/2, ROOT.kRed+1, 20)
+
+    DoomsDay  = cPlanet('DoomsDay',    -1, X0, Y0, Z0, 0.5*kEarthMass,  40.*kms,  0.8*kAU, theta, 3*pi/2, ROOT.kGray+1, 20)
 
     # outer:
-    Jupiter = cPlanet('Jupiter', 5, X0, Y0, Z0, 317.8*kEarthMass, 8.1*kms,  5.20*kAU, 0.,  pi/3, ROOT.kRed, 20, 2.)
-    Saturn  = cPlanet('Saturn',  6, X0, Y0, Z0,  95.2*kEarthMass, 6*kms,    9.58*kAU, 0., -pi/3, ROOT.kRed, 20, 2.)
-    Uranus  = cPlanet('Uranus',  7, X0, Y0, Z0,  14.5*kEarthMass, 4.2*kms, 19.20*kAU, 0.,  pi,   ROOT.kRed, 20, 2.)
-    Neptune = cPlanet('Neptune', 8, X0, Y0, Z0,  17.1*kEarthMass, 3.4*kms, 30.05*kAU, 0., -pi,   ROOT.kRed, 20, 2.)
+    Jupiter = cPlanet('Jupiter', 5, X0, Y0, Z0, 317.8*kEarthMass, 13.1*kms,  5.20*kAU, theta,  pi/3, ROOT.kPink, 20)
+    Saturn  = cPlanet('Saturn',  6, X0, Y0, Z0,  95.2*kEarthMass,  9.7*kms,    9.58*kAU, theta, -pi/3, ROOT.kMagenta, 20)
+    Uranus  = cPlanet('Uranus',  7, X0, Y0, Z0,  14.5*kEarthMass,  6.8*kms, 19.20*kAU, theta,  pi,   ROOT.kBlue, 20)
+    Neptune = cPlanet('Neptune', 8, X0, Y0, Z0,  17.1*kEarthMass,  5.4*kms, 30.05*kAU, theta, -pi,   ROOT.kCyan, 20)
     
-    planets = [Sun, Mercury, Venus, Earth, Mars, Jupiter]
+    #planets = [Sun, Mercury, Venus, Earth, Mars, DoomsDay]
+    planets = [Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, DoomsDay]
 
-    system = cSystem("Solar", X0, Y0, Z0)
     system.planets = planets
     allok = system.CheckUniquePlanetsNamesOK(planets)
     print('System planet names are OK: {}'.format(allok))
@@ -170,55 +267,152 @@ def MakeSolarSystem():
     return system
 
 
+########################################################################################
+def MakeBinary():
+
+    # phi's are random
+    # theta is 0
+
+    relSF = 3.
+    SF = relSF * kAU
+    
+    X0 = relSF/2.*kAU
+    Y0 = relSF/2.*kAU
+    Z0 = 0.0*kAU
+    
+    system = cSystem("Binary", X0, Y0, Z0, SF, 400, 0)
+    
+    theta = pi/2.
+    
+    # the Suns of the binary system
+    Sun1 = cPlanet('Sun1', 0, X0, Y0, Z0, 1.*kSunMass,  20.*kms, 0.5*kAU, theta, -pi/2, ROOT.kRed, 20, 0.3)
+    Sun2 = cPlanet('Sun2', 1, X0, Y0, Z0, 1.*kSunMass,  20.*kms, 0.5*kAU, theta, +pi/2, ROOT.kBlue, 20, 0.3)
+
+    planets = [Sun1, Sun2]
+
+    system.planets = planets
+    allok = system.CheckUniquePlanetsNamesOK(planets)
+    print('System planet names are OK: {}'.format(allok))
+    
+    return system
+
+
+
+########################################################################################
+def MakeTertiary():
+
+    # phi's are random
+    # theta is 0
+
+    relSF = 50.
+    SF = relSF * kAU
+    
+    X0 = relSF/2.*kAU
+    Y0 = relSF/2.*kAU
+    Z0 = 0.0*kAU
+    
+    system = cSystem("Tertiary", X0, Y0, Z0, SF, 800, 0)
+    
+    theta = pi/2.
+    
+    # the Suns of the binary system
+    Sun1 = cPlanet('Sun1', 0, X0, Y0, Z0, 1.*kSunMass, 12.*kms, 5*kAU, theta, -pi/2, ROOT.kRed, 20, 0.3)
+    Sun2 = cPlanet('Sun2', 1, X0, Y0, Z0, 1.*kSunMass, 12.*kms, 5*kAU, theta, +pi/2, ROOT.kBlue, 20, 0.3)
+    Sun3 = cPlanet('Sun2', 1, X0, Y0, Z0, 1.*kSunMass, 2.*kms, 1.*kAU, theta, pi, ROOT.kBlack, 20, 0.3)
+
+    planets = [Sun1, Sun2, Sun3]
+
+    system.planets = planets
+    allok = system.CheckUniquePlanetsNamesOK(planets)
+    print('System planet names are OK: {}'.format(allok))
+    
+    return system
+
+
+
+
 ########################################################################################        
 
-def GetForce(p1, p2):
+def GetForce(p1, p2, debug = 0):
     f = [0., 0., 0.]
     dR2 = 0.
     vect = [0., 0., 0.]
     for i in range(0,kdim):
         dR2 = dR2 + pow(p1.x[i] - p2.x[i], 2)
-        vect[i] = p1.x[i] - p2.x[i]
+        vect[i] = -p1.x[i] + p2.x[i]
     if dR2 > 0:
         # todo: make sure the attractive force;-)
         # F ~ 1/dR^2, direct: F ~ R / dR^3, dR^3 = dR2^(3/2)
+        if debug:
+            print('dR: AU', sqrt(dR2) / kAU)
+            print('vect: {}, {}, {} AU'.format(vect[0]/kAU, vect[1]/kAU, vect[2]/kAU))
         for i in range(0,kdim):
+            if debug:
+                print('m1={}, m2={} kappa*m1*m2={} kappa*m1*m2/dR2={}'.format(p1.mass, p2.mass, kappa * p1.mass * p2.mass, 1. / dR2 * kappa * p1.mass * p2.mass ))
+            ### CORE PHYSICS
             f[i] = vect[i] / pow(dR2, 3./2.) * kappa * p1.mass * p2.mass
+        if debug:
+            print('force: {}, {}, {} MZ', f[0]/kEarthMass, f[1]/kEarthMass, f[2]/kEarthMass, )
+    else:
+        print('ERROR getting the force!')
     return f
 
 ########################################################################################
 def ComputeForceVect(planet, planets):
-
     f = [0., 0., 0]
     for p in planets:
         if p.pid == planet.pid:
             continue
         force = GetForce(planet, p)
         for i in range(0, kdim):
+            ### CORE superposition of forces
             f[i] = f[i] + force[i]
     
     return f
 
 ########################################################################################
-def MakeStep(planet, planets, dt):
-
-    F = ComputeForceVect(planet, planets)
-
+def MakeStep(planet, system, dt, debug = 0):
+    if debug:
+        print('-------------- MakeStep {} --------------'.format(planet.name))
+    F = ComputeForceVect(planet, system.planets)
+    if debug:
+        print('original v: {}, {}, {} km/s'.format(planet.v[0]/kms, planet.v[1]/kms, planet.v[2]/kms) )
     dp = [0., 0., 0.]
     for i in range(0, kdim):
         # Mr. Newton
+        ### CORE PHYSICS
         dp[i] = F[i]*dt
+        if debug:
+            print('   dp{} = {}'.format(i, dp[i]))
         # dx = v*dt
-        planet.x[i] = planet.x[i] + dp[i] / planet.mass * dt
+        ### CORE PHYSICS
+        planet.x[i] = planet.x[i] +  planet.v[i]*dt + dp[i] / planet.mass * dt
+        ### CORE PHYSICS
         planet.v[i] = planet.v[i] + dp[i] / planet.mass
-        
+    ###!!! planet.RecomputePolar()
+    # and if use above, remove next line!
+    planet.Archive()
+    planet.time = planet.time + dt
+    
+    if planet.name != kSunName and not planet.timePrinted and GetDr(planet.x, planet.x0) < kepsilon and planet.time > 10*kday:
+        # Wow, we have a revolution!
+        T = planet.time / (24*3600.)
+        print('planet {} T={} days'.format(planet.name, T))
+        planet.timePrinted = True
+    
+    if debug:
+        print('updated v: {}, {}, {} km/s'.format(planet.v[0]/kms, planet.v[1]/kms, planet.v[2]/kms) )
     return
 
 ########################################################################################
 
-def MovePlanets(planets, dt = 1.e-4):
-    for planet in planets:
-        MakeStep(planet, planets, dt)
+def MovePlanets(system, dt, debug = 0):
+    for planet in system.planets:
+        MakeStep(planet, system, dt)
+        if debug:
+            planet.Print()
+    system.Inc()
+    #print('iteration: {}'.format(system.iteration))
 
 ########################################################################################
 ########################################################################################
@@ -264,18 +458,30 @@ def main(argv):
     print('*** Settings:')
     print('tag={:}, batch={:}'.format(gTag, gBatch))
 
-    canname = 'can'
-    can = ROOT.TCanvas(canname, canname)
-    cans.append(can)
+    systems = [ MakeSolarSystem(), MakeBinary(), MakeTertiary() ]
 
-    system = MakeSolarSystem()
+    for system in systems:
+        system.Print()
 
-    Nsteps = 100
-    dt = 1e-4
+    Nsteps = 10000
+    dt = 0.1*kday
     for istep in range(0, Nsteps):
-        MovePlanets(system.planets, dt)
-        system.Draw()
-        
+        for system in systems:
+            MovePlanets(system, dt)
+            system.DrawTracks()
+
+    # more steps for the tertiry system:
+    for istep in range(Nsteps, 10*Nsteps):
+        for system in systems[-1:]:
+            MovePlanets(system, dt)
+            system.DrawTracks()
+
+            
+    stuff.append(systems)
+
+    for system in systems:
+        system.can.Print('final_' + system.name + '.png')
+    
     
     ROOT.gApplication.Run()
     return
