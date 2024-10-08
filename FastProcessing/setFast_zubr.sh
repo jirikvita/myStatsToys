912e8f0bd020df69accd86c183edfbd708a439cb
Jiri Kvita <jiri.kvita@gmail.com>
2024-10-08 21:44:31 +0200

    2Ds

---
+++ FastProcessing/setFast_zubr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# source ROOT
+#source /home/hamal/Projects/root_v6.32.04/bin/thisroot.sh
+source /opt/ROOT/v6.26.06/bin/thisroot.sh
+
+# set FAST framework - set FAST_SW
+#export FAST_ROOT=$HOME/Projects/FAST/example/FastFramework
+
+export FAST_ROOT=/home/qitek/work/FAST/featuresExtract/FastFramework
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$FAST_ROOT/lib
